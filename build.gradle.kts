f078afc86f4bbb0201e5a9278553899e9adcad0b
vitekkor <viktor.korotkikh@gmail.com>
2024-04-01 13:02:42 +0300

    add gradle infrastructure

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "1.9.22" apply false
+}
+
+allprojects {
+    group = "com.memedb"
+    version = "1.0.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
